422c7cfa2cc506df85ac233803de3d5516a4673d
andy <cd_wjp@163.com>
2022-07-27 22:06:54 +0800

    CR

--- src/main/java/com/findthinks/delay/job/share/lib/enums/ExceptionEnum.java
+++ src/main/java/com/findthinks/delay/job/share/lib/enums/ExceptionEnum.java
@@ -5,13 +5,13 @@ package com.findthinks.delay.job.share.lib.enums;
  */
 public enum ExceptionEnum {
 
-    SUCCESS("ok", "Success."),
-    INVALID_PARAMS("invalid_params", "Parameter error"),
-    USERNAME_PWD_ERROR("username_pwd_error","username or password error."),
-    AUTHENTICATION_NOT_EXIST("authentication_not_exist", "User not logged in."),
-    CANNOT_CANCEL_JOB("cannot_cancel_job", "Cannot cancel job."),
-    JOB_IS_CANCEL("job_is_cancel", "Job is cancel."),
-    UNKNOWN_ERROR( "unknown_error", "Unknown error.");
+    SUCCESS("ok", "success"),
+    INVALID_PARAMS("invalid_params", "parameter error"),
+    USERNAME_PWD_ERROR("username_pwd_error","username or password error"),
+    AUTHENTICATION_NOT_EXIST("authentication_not_exist", "user not logged in"),
+    CANNOT_CANCEL_JOB("cannot_cancel_job", "cannot cancel job"),
+    JOB_IS_CANCEL("job_is_cancel", "job is cancel"),
+    UNKNOWN_ERROR( "unknown_error", "unknown error");
 
     /** 枚举编码 */
     private String code;
