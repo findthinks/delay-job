ec3c118987bc65689f60a26afecace42d4878aea
bbfisher <ybborjxy@126.com>
2022-10-27 22:11:35 +0800

    CR

--- docs/db/schema_init.sql
+++ docs/db/schema_init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE `delay_job` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `delay_job`;
 
+DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
 `id` int NOT NULL AUTO_INCREMENT,
 `account` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
@@ -9,38 +10,53 @@ CREATE TABLE `account` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `global_rec`;
 CREATE TABLE `global_rec` (
 `id` int unsigned NOT NULL AUTO_INCREMENT,
 `out_job_no` varchar(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '外部任务编号',
-`job_shard_id` int unsigned NOT NULL COMMENT '任务分片ID',
+`job_shard_id` tinyint unsigned NOT NULL COMMENT '任务分片ID',
 `job_id` bigint NOT NULL COMMENT '内部任务ID',
 `trigger_time` bigint NOT NULL COMMENT '触发时间',
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 PRIMARY KEY (`id`),
 UNIQUE KEY `idx_global_out_job_no` (`out_job_no`) USING BTREE,
 KEY `idx_global_trigger_time` (`trigger_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `job_seg_trigger`;
+CREATE TABLE `job_seg_trigger` (
+`id` int unsigned NOT NULL AUTO_INCREMENT,
+`job_shard_id` tinyint unsigned NOT NULL COMMENT '任务分片ID',
+`trigger_time_start` bigint NOT NULL COMMENT '触发段起始时间',
+`trigger_time_end` bigint NOT NULL COMMENT '触发段截止时间',
+`gmt_modify` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后修改时间',
+PRIMARY KEY (`id`),
+KEY `idx_st_jshard_id` (`job_shard_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `job_seg_trigger_flow`;
 CREATE TABLE `job_seg_trigger_flow` (
 `id` int unsigned NOT NULL AUTO_INCREMENT,
-`job_shard_id` int unsigned NOT NULL COMMENT '任务分片ID',
+`job_shard_id` tinyint unsigned NOT NULL COMMENT '任务分片ID',
 `trigger_time_start` bigint NOT NULL COMMENT '任务段触发起始时间',
 `trigger_time_end` bigint NOT NULL COMMENT '任务段截止时间',
-`state` int NOT NULL COMMENT '任务段处理状态:20-执行中 30-补偿中 40-完成',
-`retry_times` int unsigned NOT NULL DEFAULT '0' COMMENT '重试次数',
+`state` tinyint NOT NULL COMMENT '任务段处理状态:20-执行中 30-补偿中 40-完成',
+`retry_times` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '重试次数',
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 PRIMARY KEY (`id`),
 KEY `idx_sgf_shard_trigger_time` (`job_shard_id`,`trigger_time_start`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3167 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `job_shard`;
 CREATE TABLE `job_shard` (
- `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '分片ID',
- `cur_server` int NOT NULL COMMENT '当前分片持有者',
- `req_server` int NOT NULL DEFAULT '0' COMMENT '分片申请者',
- `state` int unsigned NOT NULL COMMENT '5-正常，10-任务转移中，15-停用',
- PRIMARY KEY (`id`)
+`id` tinyint unsigned NOT NULL AUTO_INCREMENT COMMENT '分片ID',
+`cur_server` int NOT NULL COMMENT '当前分片持有者',
+`req_server` int NOT NULL DEFAULT '0' COMMENT '分片申请者',
+`state` tinyint unsigned NOT NULL COMMENT '5-正常，10-任务转移中，15-停用',
+PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `scheduler_info`;
 CREATE TABLE `scheduler_info` (
 `id` int unsigned NOT NULL AUTO_INCREMENT,
 `uuid` varchar(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '调度器ID',
@@ -49,7 +65,7 @@ CREATE TABLE `scheduler_info` (
 `ip` varchar(25) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '调度器ip',
 PRIMARY KEY (`id`),
 KEY `idx_scheduler_uuid` (`uuid`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `sequence_key`;
 CREATE TABLE `sequence_key` (
